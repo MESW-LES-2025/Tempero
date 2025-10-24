b2df0f5821e535fa89de7c68dd2896edf928b80a
Beatriz Bernardo <beatrizxalexandra@gmail.com>
2025-10-24 16:27:24 +0100

    login working

--- tempero/src/App.tsx
+++ tempero/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 
 function NotFoundPage() {
@@ -10,17 +11,18 @@ function NotFoundPage() {
 export default function App() {
   return (
     <BrowserRouter>
-      {/* 🧭 Simple navbar just to navigate between routes */}
+      {/* Simple navbar just to navigate between routes */}
       <nav className="flex justify-center gap-4 p-4 border-b bg-gray-100">
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/register" className="hover:underline">Register</Link>
         <Link to="/login" className="hover:underline">Login</Link>
       </nav>
 
-      {/* 🧱 Page routes */}
+      {/* Page routes */}
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} /> 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
