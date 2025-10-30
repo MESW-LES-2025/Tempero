96717a39ac8a7d4fa9219a34c2eefc3604da2365
almahmudsarker <almahmuds427@gmail.com>
2025-10-30 23:20:36 +0000

    profile page

--- tempero/src/App.tsx
+++ tempero/src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
 
 function NotFoundPage() {
   return <div className="p-6 text-center">404 - Page not found</div>;
@@ -13,16 +13,26 @@ export default function App() {
     <BrowserRouter>
       {/* Simple navbar just to navigate between routes */}
       <nav className="flex justify-center gap-4 p-4 border-b bg-gray-100">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/register" className="hover:underline">Register</Link>
-        <Link to="/login" className="hover:underline">Login</Link>
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/register" className="hover:underline">
+          Register
+        </Link>
+        <Link to="/login" className="hover:underline">
+          Login
+        </Link>
+        <Link to="/profile" className="hover:underline">
+          Profile
+        </Link>
       </nav>
 
       {/* Page routes */}
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="register" element={<RegisterPage />} />
-        <Route path="login" element={<LoginPage />} /> 
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
