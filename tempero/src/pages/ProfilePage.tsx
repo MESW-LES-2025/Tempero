1cf337cf110b43fcf9760b9b6c2e10898780a0ac
almahmudsarker <almahmuds427@gmail.com>
2025-11-06 16:30:21 +0000

    edit profile button added

--- tempero/src/pages/ProfilePage.tsx
+++ tempero/src/pages/ProfilePage.tsx
@@ -55,6 +55,11 @@ export default function ProfilePage() {
           a humble sauce to a timeless creation like Peach Melba. For me cooking
           is not just a craft, it is harmony, precision and pure joy.
         </p>
+
+        {/* Edit profile button */}
+        <button className="mt-6 w-full bg-[#e57f22] hover:bg-[#cf6e1d] text-white text-sm sm:text-base font-medium py-2.5 rounded-md transition-colors">
+          Edit Profile
+        </button>
       </article>
 
       {/* R8 */}
