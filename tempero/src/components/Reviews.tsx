ca2360ec1ee872c14eba64211426fbb73b203fa1
almahmudsarker <almahmuds427@gmail.com>
2025-11-07 22:06:52 +0000

    chore: save progress on profile page integration

--- tempero/src/components/Reviews.tsx
+++ tempero/src/components/Reviews.tsx
@@ -1,7 +1,5 @@
-type ReviewsProps = {
-  userId?: string;
-  username?: string;
-};
+import { useEffect, useState } from "react";
+import { supabase } from "../config/supabaseClient";
 
 type Review = {
   id: number;
@@ -10,32 +8,26 @@ type Review = {
   comment: string;
 };
 
-export default function Reviews({ userId, username }: ReviewsProps) {
-  console.log("Reviews filtering by:", userId, username);
+export default function Reviews() {
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [loading, setLoading] = useState(true);
 
-const reviews: Review[] = [
-  {
-    id: 1,
-    author: "Julia Child",
-    rating: 2,
-    comment:
-      "Auguste Escoffier revolutionized the way we perceive cooking. His precision and simplicity inspire chefs even today.",
-  },
-  {
-    id: 2,
-    author: "Gordon Ramsay",
-    rating: 5,
-    comment:
-      "A master of discipline and creativity — Escoffier’s techniques define culinary professionalism.",
-  },
-  {
-    id: 3,
-    author: "Heston Blumenthal",
-    rating: 4,
-    comment:
-      "Brilliant mind and timeless influence. His philosophy of balance and respect for ingredients shaped modern gastronomy.",
-  },
-];
+  useEffect(() => {
+    const fetchReviews = async () => {
+      const { data, error } = await supabase
+        .from("reviews")
+        .select(
+          `id,author_id,profiles(first_name, last_name),review,description,recipe_id,recipes(title)`
+        )
+        .order("id", { ascending: false });
+      if (error) console.error("Error fetching reviews:", error);
+      else setReviews(data as Review[]);
+      setLoading(false);
+    };
+    fetchReviews();
+  }, []);
+
+  if (loading) return <p>Loading reviews...</p>;
 
   return (
     <div className="space-y-4">
@@ -45,15 +37,17 @@ const reviews: Review[] = [
           className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm hover:shadow-md transition"
         >
           <div className="flex items-center justify-between">
-            <h4 className="font-semibold text-[#e57f22]">{rev.author}</h4>
+            <h4 className="font-semibold text-[#e57f22]">
+              {rev.profiles.first_name} {rev.profiles.last_name}
+            </h4>
+            <h5 className="text-gray-500 text-sm">{rev.recipes.title}</h5>
             <span className="text-yellow-500">
-              {"★".repeat(rev.rating)}
-              {"☆".repeat(5 - rev.rating)}
+              {"★".repeat(rev.review)}
+              {"☆".repeat(5 - rev.review)}
             </span>
           </div>
-
           <p className="mt-2 text-gray-700 text-sm leading-relaxed">
-            {rev.comment}
+            {rev.description}
           </p>
         </article>
       ))}
